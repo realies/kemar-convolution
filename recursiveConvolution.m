a51f058364586cf705fd83d40d29b9eafea26558
realies <github@reali.es>
2018-03-02 22:10:35 +0000

    add comments

--- recursiveConvolution.m
+++ recursiveConvolution.m
@@ -6,6 +6,7 @@ nBits = 16;
 targetDir = './kemar/elev-10/';
 targetFiles = dir(fullfile(targetDir, strcat(channels(1), '*.wav')));
 
+% impulse response pairing
 impulseResponsePairs = {}; % todo: preallocate memory based on length(targetFiles)
 for i = 1:length(targetFiles)
 	fileName = targetFiles(i).name;
@@ -22,9 +23,10 @@ t = 0 : 1/Fs : .2;
 f = 1000;
 A = .5;
 w = 0 * pi/180; % degrees
-y = A * sin( 2 * pi * f * t + w );
+y = A * sin(2 * pi * f * t + w);
 sourceAudio = y;
 
+% recursive convolution
 outputAudio = []; % todo: preallocate memory based on size(impulseResponsePairs, 1)
 for i = 1:size(impulseResponsePairs, 1)
 	[fl,Fsl] = audioread(fullfile(targetDir, impulseResponsePairs{i, 1}));
